78e845c3e338ed5b555c4df268902d8036dbb924
Brandon <brandon@friendlystudios.net>
2024-11-07 16:03:35 -0600

    Testing precompiled version

--- flake.nix
+++ flake.nix
@@ -7,31 +7,27 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          config = {
-            allowUnfree = true;
-          };
-        };
-
-        commonArgs = {
-          inherit (pkgs)
-            mono
-            dotnet-sdk_8
-            dotnet-runtime_8
-            mkNugetDeps;  # Add this
-          speech-dispatcher = null;
-        };
-      in
-      {
-        packages = rec {
-          godot4_4_dev3 = pkgs.callPackage ./packages/godot/godot4_4_dev3.nix {
-            speech-dispatcher = null;
-          };
-          godot4_4_dev3_mono = pkgs.callPackage ./packages/godot/godot4_4_dev3_mono.nix commonArgs;
-          default = godot4_4_dev3;
-        };
-      }
+  pkgs = import nixpkgs {
+    inherit system;
+    config = {
+      allowUnfree = true;
+    };
+  };
+in
+{
+  packages = rec {
+    godot4_4_dev3 = pkgs.callPackage ./packages/godot/godot4_4_dev3.nix {
+      speech-dispatcher = null;
+    };
+    godot4_4_dev3_mono = pkgs.callPackage ./packages/godot/godot4_4_dev3_mono.nix {
+      mono = pkgs.mono;
+      dotnet-sdk_8 = pkgs.dotnet-sdk_8;
+      dotnet-runtime_8 = pkgs.dotnet-runtime_8;
+      speech-dispatcher = null;
+    };
+    default = godot4_4_dev3;
+  };
+}
     ) // {
       overlays.default = final: prev: let
         pkgs = import nixpkgs {
