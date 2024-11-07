31a91606ac584e0e92e0c3906f01c6d93a0d2357
Brandon <brandon@friendlystudios.net>
2024-11-07 16:04:48 -0600

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,54 +7,30 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-  pkgs = import nixpkgs {
-    inherit system;
-    config = {
-      allowUnfree = true;
-    };
-  };
-in
-{
-  packages = rec {
-    godot4_4_dev3 = pkgs.callPackage ./packages/godot/godot4_4_dev3.nix {
-      speech-dispatcher = null;
-    };
-    godot4_4_dev3_mono = pkgs.callPackage ./packages/godot/godot4_4_dev3_mono.nix {
-      mono = pkgs.mono;
-      dotnet-sdk_8 = pkgs.dotnet-sdk_8;
-      dotnet-runtime_8 = pkgs.dotnet-runtime_8;
-      speech-dispatcher = null;
-    };
-    default = godot4_4_dev3;
-  };
-}
-    ) // {
-      overlays.default = final: prev: let
         pkgs = import nixpkgs {
-          system = prev.system;
-          config.allowUnfree = true;
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
         };
-
-        commonArgs = {
-          inherit (pkgs)
-            mono
-            dotnet-sdk_8
-            dotnet-runtime_8
-            mkNugetDeps;  # Add this
-          speech-dispatcher = null;
+      in
+      {
+        packages = rec {
+          godot4_4_dev3 = pkgs.callPackage ./packages/godot/godot4_4_dev3.nix { };
+          godot4_4_dev3_mono = pkgs.callPackage ./packages/godot/godot4_4_dev3_mono.nix {
+            inherit (pkgs) mono dotnet-sdk_8 dotnet-runtime_8;
+          };
+          default = godot4_4_dev3;
         };
-      in {
-        godot4_4_dev3 = pkgs.callPackage ./packages/godot/godot4_4_dev3.nix {
-          speech-dispatcher = null;
-        };
-        godot4_4_dev3_mono = pkgs.callPackage ./packages/godot/godot4_4_dev3_mono.nix commonArgs;
+      }
+    ) // {
+      overlays.default = final: prev: {
+        godot4_4_dev3 = self.packages.${prev.system}.godot4_4_dev3;
+        godot4_4_dev3_mono = self.packages.${prev.system}.godot4_4_dev3_mono;
       };
-
-      homeManagerModules.default = { config, pkgs, ... }: {
+      
+      homeManagerModules.default = { pkgs, ... }: {
         nixpkgs.overlays = [ self.overlays.default ];
-        nixpkgs.config = {
-          allowUnfree = true;
-        };
       };
     };
 }
