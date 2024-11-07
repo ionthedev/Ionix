a7d3204015a27292c4ba392c7c8e53654ddd383d
Brandon <brandon@friendlystudios.net>
2024-11-07 16:08:41 -0600

    Update godot4_4_dev3_mono.nix

--- packages/godot/godot4_4_dev3_mono.nix
+++ packages/godot/godot4_4_dev3_mono.nix
@@ -2,14 +2,14 @@
 , stdenv
 , fetchurl
 , makeWrapper
-, unzip  # Add this
+, unzip
 , mono
 , dotnet-sdk_8
 , dotnet-runtime_8
 }:
 
 stdenv.mkDerivation rec {
-  pname = "godot4-mono";
+  pname = "godot4-4-dev3-mono";  # Changed from godot4-mono to be more specific
   version = "4.4-dev3";
 
   src = fetchurl {
@@ -19,7 +19,7 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ 
     makeWrapper 
-    unzip  # Add this
+    unzip
   ];
 
   buildInputs = [ 
@@ -41,14 +41,14 @@ stdenv.mkDerivation rec {
 
     # Install the binary and data directory
     cp -r Godot_v4.4-dev3_mono_linux_x86_64/* $out/bin/
-    mv $out/bin/Godot_v4.4-dev3_mono_linux.x86_64 $out/bin/godot4-mono
+    mv $out/bin/Godot_v4.4-dev3_mono_linux.x86_64 $out/bin/godot4-4-dev3-mono  # Changed binary name
 
     # Create desktop entry
-    cat > "$out/share/applications/godot4-mono.desktop" << EOF
+    cat > "$out/share/applications/godot4-4-dev3-mono.desktop" << EOF  # Changed desktop file name
     [Desktop Entry]
-    Name=Godot Engine 4.4 (Mono)
+    Name=Godot Engine 4.4-dev3 (Mono)
     Comment=Multi-platform 2D and 3D game engine with a feature-rich editor
-    Exec=$out/bin/godot4-mono
+    Exec=$out/bin/godot4-4-dev3-mono
     Icon=godot
     Terminal=false
     Type=Application
@@ -56,7 +56,7 @@ stdenv.mkDerivation rec {
     EOF
 
     # Wrap the binary to set required environment variables
-    wrapProgram $out/bin/godot4-mono \
+    wrapProgram $out/bin/godot4-4-dev3-mono \
       --prefix PATH : ${lib.makeBinPath [ mono dotnet-sdk_8 dotnet-runtime_8 ]} \
       --set DOTNET_ROOT ${dotnet-sdk_8} \
       --set GODOT_MONO_PREFIX $out/bin
@@ -66,9 +66,9 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     homepage = "https://godotengine.org";
-    description = "Free and Open Source 2D and 3D game engine with Mono/C# support";
+    description = "Free and Open Source 2D and 3D game engine with Mono/C# support (4.4-dev3)";
     license = licenses.mit;
     platforms = [ "x86_64-linux" ];
-    mainProgram = "godot4-mono";
+    mainProgram = "godot4-4-dev3-mono";
   };
 }
